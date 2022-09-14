ee573b5be0eb08723213c43c650782b4bdf38f37
JonathanBlobs <jow258@hotmail.com>
2022-09-14 00:12:08 -0300

    criando a página principal

--- Web/tailwind.config.cjs
+++ Web/tailwind.config.cjs
@@ -7,9 +7,20 @@ module.exports = {
   ],
 
   theme: {
+    fontFamily: {
+
+      sans: ['Inter','sans-serif'],
+
+    },
+
     extend: {
+
       backgroundImage: {
-        galaxy: "url('/fundo.galaxy.png')"
+        galaxy: "url('/fundo.galaxy.png')",
+        'nlw-gradient': 'linear-gradient(98.86deg, #9572FC 23.08%, #43E7AD 47.57%, #E1D55D  30.94%)',
+        'game-gradient': 'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.08%)',
+        
+        
       },
     },
   },
