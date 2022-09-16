22246fce78467b25c22e1606d694c5b6d949d3df
JonathanBlobs <jow258@hotmail.com>
2022-09-15 23:05:50 -0300

    obtendo listas de jogos da API

--- Web/src/App.tsx
+++ Web/src/App.tsx
@@ -1,9 +1,28 @@
+import { useState, useEffect } from 'react';
 import './styles/main.css';
 import logoImg from './assets/Logo.svg';
 import { GameBanner } from './components/GameBanner';
 import { CreateAdBanner } from './components/CreateAdBanner';
 
+interface Game {
+  id: string;
+  title: string;
+  bannerUrl: string;
+  _count: {
+    ads: number;
+  }
+}
+
 function App() {
+  const [games, setGames] = useState<Game[]>([])
+
+  useEffect(() => {
+    fetch('http://localhost:3333/games')
+      .then(response => response.json())
+      .then(data => {
+        setGames(data)
+      })
+  }, [])
 
   return (
 
@@ -14,13 +33,21 @@ function App() {
       <h1 className='text-6xl text-white font-black mt-20'>Seu <span className='text-transparent bg-nlw-gradient bg-clip-text'>duo </span>está aqui.</h1>
 
       <div className="grid grid-cols-6 gap-6 mt-16">
-
-        <GameBanner bannerUrl="/game-1.png" title="League of Legends" adsCount={5}/>
-        
+        {games.map(game => {
+          return (
+            <GameBanner
+            key={game.id}
+              bannerUrl={game.bannerUrl}
+              title={game.title}
+              adsCount={game._count.ads}
+            />
+          )
+        })}
 
       </div>
-        <CreateAdBanner/>
-    
+
+      <CreateAdBanner />
+
     </div>
 
   )
